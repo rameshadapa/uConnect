01529fc4587ac66217aa814b666eaaaf95500279
Ramesh Adapa <rameshadapa32@gmail.com>
2015-04-06 15:54:01 +0530

    Mysql db connect class.

---
+++ dynamicdb/MyDatabase.php
@@ -0,0 +1,28 @@
+<?php
+	abstract class MyDatabase
+	{
+		protected $login;
+		protected $password;
+		protected $database;
+		protected $hostname;
+
+		public function __construct($login, $password, $database, $hostname)
+		{
+			$this->throwExceptionIfNotSet('login', $login);
+			$this->throwExceptionIfNotSet('database', $database);
+			$this->throwExceptionIfNotSet('hostname', $hostname);
+
+			$this->login = $login;
+			$this->password = $password;
+			$this->database = $database;
+			$this->hostname = $hostname;
+		}
+		private function throwExceptionIfNotSet($argName, $argValue)
+		{
+			if(empty($argValue))
+			{
+				throw new MyDBException("'${argName}' not set");
+			}
+		}
+	}
+?>
---
+++ dynamicdb/UConnectDB.php
@@ -0,0 +1,39 @@
+<?php
+	class UConnectDB extends MyDatabase
+	{
+		private $link = null;
+		
+		public function __construct($login, $password, $databse, $hostname)
+		{
+			parent::__construct($login, $password, $databse, $hostname);
+			$this->connect();
+			$this->selectDatabase();
+		}
+		public function connect()
+		{
+			if(! is_null($this->link))
+			{
+				return;
+			}
+			$link = @mysql_connect($this->hostname, $this->login, $this->password);
+			if(! $link)
+			{
+				throw new MyDBException(
+					sprintf(
+						'Cannot connect to database. mysql_connect() to %s with login %s fails',
+						$this->hostname,
+						$this->login
+					)
+				);
+			}
+		}
+		public function selectDatabase()
+		{
+			$ret = @mysql_select_db($this->database, $this->link);
+			if(! $ret)
+			{
+				throw new MyDBException('No database name %s existed', $this->database);
+			}
+		}
+	}
+?>
