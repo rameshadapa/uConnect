2b23a5ffde875897dca44c46f661fb79367985cc
Ramesh Adapa <rameshadapa32@gmail.com>
2015-04-09 22:14:46 +0530

    Init android source.

---
+++ proj.android/src/com/devkids/ubroadcast/uBroadcastAppActivity.java
@@ -0,0 +1,15 @@
+package com.devkids.ubroadcast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class uBroadcastAppActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
